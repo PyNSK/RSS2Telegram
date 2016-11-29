cc5d4128bab59afc2687b756be440e7beeab0fb8
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-11-29 10:50:56 +0700

    Update bot

--- bot.py
+++ bot.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
-import json
-import time
-from datetime import datetime
-import calendar
 import sys
-import os
-import logging
+import time
 
-from twx.botapi import TelegramBot
-import twx.botapi
-from lxml.html import fragment_fromstring
+import calendar
 import feedparser
+import json
+import logging
+import os
+import twx.botapi
+from datetime import datetime
+from lxml.html import fragment_fromstring
+from twx.botapi import TelegramBot
 
 import settings
 
 
-def removeTag(str):
+def remove_tags(text):
     """
     This function remove all html tags from string.
     <br/> tags are replaced by newline. 
 
     """
-    root = fragment_fromstring(str, create_parent='div')
+    root = fragment_fromstring(text, create_parent='div')
     for br in root.xpath("br"):
         br.tail = "\n" + br.tail if br.tail else "\n"
     return root.text_content()
@@ -32,12 +32,19 @@ def load_settings():
         with open('db.json', encoding='utf-8') as f:
             tgm_data = json.load(f)
 
-        last_post_date = datetime.strptime(tgm_data['last_post_date'], '%Y-%m-%dT%H:%M:%S')
+        last_post_date = datetime.strptime(tgm_data['last_post_date'],
+                                           '%Y-%m-%dT%H:%M:%S')
     else:
         last_post_date = datetime(1, 1, 1, 0, 0, 0)
         tgm_data = {}
 
     return last_post_date, tgm_data
+
+
+def save_settings(date):
+    tgm_data = {'last_post_date': date}
+    with open('db.json', 'w', encoding='utf-8') as f:
+        json.dump(tgm_data, f)
 
 
 def main():
@@ -50,7 +57,8 @@ def main():
     new_entries = []
     for entry in rss.entries:
         try:
-            entry_published = datetime.utcfromtimestamp(calendar.timegm(entry.published_parsed))
+            entry_published = datetime.utcfromtimestamp(
+                calendar.timegm(entry.published_parsed))
             if entry_published > last_post_date:
                 new_entries.append(entry)
         except AttributeError as e:
@@ -58,38 +66,46 @@ def main():
 
     logging.info('The number of new entries: %s\nEntries: %s',
                  len(new_entries),
-                 [(item.get('id'), item.get('published_parsed')) for item in new_entries])
+                 [(item.get('id'), item.get('published_parsed')) for item in
+                  new_entries])
 
-    if new_entries:
-        # sort new entries by published date
-        new_entries.sort(key=lambda item: item.published_parsed)
-
-        # send to telegram channel
-        tgm_bot = TelegramBot(settings.TGM_BOT_ACCESS_TOKEN)
-        for entry in new_entries:
-            try:
-
-                logging.debug("Raw message:\n%s\n", entry.description)
-                message = removeTag(entry.description)
-                logging.debug("message:\n%s\n", message)
-            except AttributeError as e:
-                logging.error("%s\n%s", e, entry)
-                continue
-
-            answer = tgm_bot.send_message(settings.TGM_CHANNEL, message).wait()
-            if isinstance(answer, twx.botapi.Error):
-                logging.error("error code: %s\nerror description: %s\n",
-                              answer.error_code,
-                              answer.description)
-                sys.exit()
-            else:
-                tgm_data['last_post_date'] = datetime.utcfromtimestamp(
-                    calendar.timegm(entry.published_parsed)).isoformat()
-                with open('db.json', 'w', encoding='utf-8') as f:
-                    json.dump(tgm_data, f)
-            time.sleep(1)
-    else:
+    date = datetime.now()
+    if not new_entries:
         logging.info('New entries are not found')
+        save_settings(date.isoformat())
+        return
+
+    # sort new entries by published date
+    new_entries.sort(key=lambda item: item.published_parsed)
+
+    # send to telegram channel
+    tgm_bot = TelegramBot(settings.TGM_BOT_ACCESS_TOKEN)
+    for entry in new_entries:
+        try:
+
+            logging.debug("Raw message:\n%s\n", entry.description)
+            message = remove_tags(entry.description)
+            logging.debug("message:\n%s\n", message)
+        except AttributeError as e:
+            logging.error("%s\n%s", e, entry)
+            continue
+
+        answer = tgm_bot.send_message(settings.TGM_CHANNEL, message).wait()
+        if isinstance(answer, twx.botapi.Error):
+            logging.error("error code: %s\nerror description: %s\n",
+                          answer.error_code,
+                          answer.description)
+            break
+        else:
+            date = max(
+                datetime.utcfromtimestamp(
+                    calendar.timegm(entry.published_parsed)),
+                date
+            )
+
+        time.sleep(1)
+
+    save_settings(date.isoformat())
 
 
 if __name__ == "__main__":
